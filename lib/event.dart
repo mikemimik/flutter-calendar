5e53bc0c92f08ae6abb257416357975db5ea2b92
afrolionx <mike@mikecorp.ca>
2016-11-19 03:08:16 -0500

    created widgets for the EventRow and EventIcon in the calendar

--- lib/event.dart
+++ lib/event.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:meta/meta.dart';
 
 class EventItem extends StatelessWidget {
   @override
@@ -8,6 +9,9 @@ class EventItem extends StatelessWidget {
 }
 
 class EventCalendarIcon extends StatelessWidget {
+  EventCalendarIcon({ @required bool this.submitted });
+  bool submitted;
+  
   // TODO: create a flag for completed or not
   @override
   Widget build(BuildContext context) {
@@ -16,7 +20,23 @@ class EventCalendarIcon extends StatelessWidget {
       width: 5.0,
       margin: new EdgeInsets.all(0.5),
       decoration: new BoxDecoration(
-        backgroundColor: Colors.green[500]
+        backgroundColor: (submitted) ? Colors.green[500] : Colors.red[500]
+      )
+    );
+  }
+}
+
+class EventCalendarRow extends StatelessWidget {
+  EventCalendarRow({ @required List<EventCalendarIcon> this.eventIcons });
+  List<EventCalendarIcon> eventIcons;
+  
+  @override
+  Widget build(BuildContext context) {
+    return new Align(
+      alignment: FractionalOffset.bottomCenter,
+      heightFactor: 2.5,
+      child: new Row(
+        children: eventIcons
       )
     );
   }
