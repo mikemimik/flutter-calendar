65eb94a6828ad99740fca10d824c9aa9db7230c3
afrolionx <mike@mikecorp.ca>
2016-11-19 01:47:42 -0500

    updated day with event class widget component

--- lib/day.dart
+++ lib/day.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:meta/meta.dart';
+import 'event.dart';
 
 class Day extends StatelessWidget {
   Day({ int this.date });
@@ -9,17 +10,35 @@ class Day extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return new Flexible(
-        child: new Container(
-            height: 60.0,
-            decoration: new BoxDecoration(
-                border: new Border.all(
-                    color: Colors.black,
-                    width: 0.5
-                )
+      child: new Container(
+        height: 60.0,
+        decoration: new BoxDecoration(
+          border: new Border.all(
+            color: Colors.black,
+            width: 0.5
+          )
+        ),
+        padding: new EdgeInsets.all(4.0),
+        child: new Column(
+          crossAxisAlignment: CrossAxisAlignment.end,
+          children: <Widget>[
+            new Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: <Widget>[
+                new Text(date.toString())
+              ],
             ),
-            padding: new EdgeInsets.all(4.0),
-            child: new Text(date.toString())
+            new Row(
+              children: <Widget>[
+                new EventCalendarIcon(),
+                new EventCalendarIcon(),
+                new EventCalendarIcon(),
+                new EventCalendarIcon(),
+              ]
+            )
+          ]
         )
+      )
     );
   }
 }
