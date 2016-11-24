a758a8a1d652c8677f29a12fb23d68881dc5dddc
afrolionx <mike@mikecorp.ca>
2016-11-24 15:55:52 -0500

    added EventView class

---
+++ lib/view.event.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:meta/meta.dart';
+import 'view_types.dart';
+
+class EventView extends StatelessWidget {
+  EventView({
+    @required int this.year,
+    @required int this.month,
+    @required int this.day,
+    @required this.switchViewCallback
+  });
+  
+  final int year;
+  final int month;
+  final int day;
+  final switchViewCallback;
+  
+  @override
+  Widget build(BuildContext context) {
+    Widget component = new Container(
+      constraints: new BoxConstraints(),
+      margin: new EdgeInsets.all(8.0),
+      child: new Center(
+        child: new Column(
+          children: <Widget>[
+            new Text('Day: ' + MonthNames[month]['long'] + '$day, $year'),
+            new Container(
+              padding: new EdgeInsets.all(8.0),
+              child: new RaisedButton(
+                child: new Text('back to calendar'),
+                onPressed: () {
+                  switchViewCallback();
+                }
+              )
+            )
+          ]
+        )
+      )
+    );
+    
+    return component;
+  }
+}
