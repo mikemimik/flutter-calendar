d181575f85afd7f4c7b8b7722c793402dbf02bca
afrolionx <mike@mikecorp.ca>
2016-11-24 14:26:32 -0500

    created view types file will all calendar wide constants

---
+++ lib/view_types.dart
@@ -0,0 +1,17 @@
+
+enum RenderableViews { calendar, event }
+
+Map<int, Map<String, String>> MonthNames = {
+  0: { 'short': 'Jan', 'long': 'January' },
+  1: { 'short': 'Feb', 'long': 'February' },
+  2: { 'short': 'Mar', 'long': 'March' },
+  3: { 'short': 'Apr', 'long': 'April' },
+  4: { 'short': 'May', 'long': 'May' },
+  5: { 'short': 'June', 'long': 'June' },
+  6: { 'short': 'July', 'long': 'July' },
+  7: { 'short': 'Aug', 'long': 'August' },
+  8: { 'short': 'Sep', 'long': 'September' },
+  9: { 'short': 'Oct', 'long': 'October' },
+  10: { 'short': 'Nov', 'long': 'November' },
+  11: { 'short': 'Dec', 'long': 'December' }
+};
