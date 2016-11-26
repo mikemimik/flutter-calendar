66f0ab3164c4edc211f8e33f714fc8994789b9e4
afrolionx <mike@mikecorp.ca>
2016-11-26 16:02:09 -0500

    EventView now takes Day class rather than int (drilling down into class can happen now)

--- lib/view.event.dart
+++ lib/view.event.dart
@@ -1,18 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:meta/meta.dart';
 import 'view_types.dart';
+import 'day.dart';
 
 class EventView extends StatelessWidget {
   EventView({
     @required int this.year,
     @required int this.month,
-    @required int this.day,
+    @required Day this.day,
     @required ViewCallback this.switchViewCallback
   });
 
   final int year;
   final int month;
-  final int day;
+  final Day day;
   final ViewCallback switchViewCallback;
 
   @override
@@ -23,7 +24,7 @@ class EventView extends StatelessWidget {
       child: new Center(
         child: new Column(
           children: <Widget>[
-            new Text('Day: ' + MonthNames[month]['long'] + ' $day, $year'),
+            new Text('Day: ' + MonthNames[month]['long'] + ' ${day}, $year'),
             new Container(
               padding: new EdgeInsets.all(8.0),
               child: new RaisedButton(
