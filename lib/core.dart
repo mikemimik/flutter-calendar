1d6d51d393a3bb0b088c78b7efd576423fe9a8da
afrolionx <mike@mikecorp.ca>
2016-11-26 23:34:42 -0500

    created core library file

---
+++ lib/core.dart
@@ -0,0 +1,16 @@
+library calendar;
+
+// CORE
+export 'calendar.dart';
+export 'month.dart';
+export 'week.dart';
+export 'day.dart';
+export 'event.dart';
+
+// UTILITIES
+export 'data.dart';
+export 'view_types.dart';
+
+// VIEWS
+export 'view.calendar.dart';
+export 'view.event.dart';
