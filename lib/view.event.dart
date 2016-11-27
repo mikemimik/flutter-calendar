230f2cf482ecf7a4caf2939ec9c1506fd37d25a8
afrolionx <mike@mikecorp.ca>
2016-11-27 00:06:44 -0500

    refactored component composition into three functions (header, eventRows, footer)

--- lib/view.event.dart
+++ lib/view.event.dart
@@ -15,6 +15,34 @@ class EventView extends StatelessWidget {
   final Day day;
   final ViewCallback switchViewCallback;
 
+  Widget _generateHeader() {
+    return new Text('Day: ' + MonthNames[month]['long'] + ' ${day.date}, $year');
+  }
+
+  Widget _generateEventRows() {
+    Column component = new Column(
+      children: <Widget>[]
+    );
+    day.getEvents().forEach((event) {
+      component.children.add(new EventRow(rowEvent: event));
+    });
+    return component;
+  }
+
+  Widget _generateFooter() {
+    return new Container(
+      padding: new EdgeInsets.all(8.0),
+      child: new RaisedButton(
+        child: new Text('back to calendar'),
+        onPressed: () {
+          switchViewCallback(
+            view: RenderableView.calendar
+          );
+        }
+      )
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     Widget component = new Container(
@@ -23,23 +51,13 @@ class EventView extends StatelessWidget {
       child: new Center(
         child: new Column(
           children: <Widget>[
-            new Text('Day: ' + MonthNames[month]['long'] + ' ${day.date}, $year'),
-            new Container(
-              padding: new EdgeInsets.all(8.0),
-              child: new RaisedButton(
-                child: new Text('back to calendar'),
-                onPressed: () {
-                  switchViewCallback(
-                    view: RenderableView.calendar
-                  );
-                }
-              )
-            )
+            _generateHeader(),
+            _generateEventRows(),
+            _generateFooter()
           ]
         )
       )
     );
-
     return component;
   }
 }
