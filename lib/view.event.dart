e8d9c31dbbb4a90b8080ae8a522222ed5c583e7d
afrolionx <mike@mikecorp.ca>
2016-12-06 11:01:02 -0500

    flushed out default EventView (singular)

--- lib/view.event.dart
+++ lib/view.event.dart
@@ -13,8 +13,41 @@ class EventView extends StatelessWidget {
   final Day day;
   final ViewCallback switchViewCallback;
 
+  Widget _generateEventViewHeader() {
+    Widget component = new Container(
+      margin: new EdgeInsets.only(top: 12.0),
+      child: new Text('${event.title}')
+    );
+    return component;
+  }
 
-  Widget _generateEventsViewFooter() {
+  Widget _generateEventViewBody() {
+    Widget component = new Column(
+      children: <Widget>[
+        _generateEventViewBodyItem('${MonthNames[event.month - 1]["long"]} ${event.day}, ${event.year}'),
+        _generateEventViewBodyItem('Start Time: ${event.getStartTime(military: false)}'),
+        _generateEventViewBodyItem('End Time: ${event.getEndTime(military: false)}'),
+        new Divider(),
+        _generateEventViewBodyItem('${event.url}'),
+        new Divider(),
+        _generateEventViewBodyItem('${event.details}')
+      ]
+    );
+    return component;
+  }
+
+  Widget _generateEventViewBodyItem(String text) {
+    Widget component = new Container(
+      margin: new EdgeInsets.only(right: 8.0, left: 8.0),
+      child: new Align(
+        alignment: FractionalOffset.centerLeft,
+        child: new Text(text)
+      )
+    );
+    return component;
+  }
+
+  Widget _generateEventViewFooter() {
     Widget component = new Container(
       padding: new EdgeInsets.all(8.0),
       margin: new EdgeInsets.only(top: 8.0, bottom: 12.0),
@@ -37,7 +70,9 @@ class EventView extends StatelessWidget {
       child: new Center(
         child: new Column(
           children: <Widget>[
-            _generateEventsViewFooter()
+            _generateEventViewHeader(),
+            _generateEventViewBody(),
+            _generateEventViewFooter()
           ]
         )
       )
