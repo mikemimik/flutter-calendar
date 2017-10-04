8bfa7898512e9faa07b4ae483025b8e98012ab78
mikemimik <mike@mikecorp.ca>
2017-10-04 14:31:09 -0400

    added calendar view widget

---
+++ lib/src/calendar/calendar_view.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+
+import 'package:calendar/src/month/month.dart';
+import 'package:calendar/controllers.dart';
+import 'package:calendar/utils.dart';
+
+class CalendarView extends StatelessWidget {
+  CalendarView({
+    @required this.year,
+    @required this.month,
+    @required this.date,
+    @required this.monthController,
+  });
+
+  final int year;
+  final int month;
+  final int date;
+  final MonthController monthController;
+
+  @override
+  Widget build(BuildContext context) {
+    return new Container(
+      child: new Column(
+        children: <Widget>[
+          new CalendarViewHeader(monthName: MonthNames[month - 1]['long']),
+          new Month(
+            year: year,
+            month: month,
+            date: date,
+            controller: monthController,
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class CalendarViewHeader extends StatelessWidget {
+  CalendarViewHeader({
+    @required this.monthName,
+  });
+
+  final String monthName;
+  @override
+  Widget build(BuildContext context) {
+    return new Container(
+      height: 40.0,
+      margin: const EdgeInsets.only(top: 5.0, bottom: 10.0),
+      child: new Align(
+        alignment: FractionalOffset.center,
+        child: new Text(monthName),
+      ),
+    );
+  }
+}
