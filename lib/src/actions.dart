4f6ad477d5a295461b9dfc8003a1689030e53372
mikemimik <mike@mikecorp.ca>
2017-10-06 01:58:05 -0400

    add Action and Trigger for removing an event

--- lib/src/actions.dart
+++ lib/src/actions.dart
@@ -6,6 +6,7 @@ import 'package:calendar/src/types.dart';
 final Action<RenderableView> switchViewAction = new Action<RenderableView>();
 final Action<Map<String, int>> selectDateAction = new Action<Map<String, int>>();
 final Action<Map<String, dynamic>> fetchDataAction = new Action<Map<String, dynamic>>();
+final Action<int> removeEventAction = new Action<int>();
 
 final Action<CalendarController> setCalendarControllerAction = new Action<CalendarController>();
 final Action<DataController> setDataControllerAction = new Action<DataController>();
